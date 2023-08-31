572ade6b6dd010826243990480b99fed9f3687a9
ilham.fathoni <ilham.fathoni>
2023-08-31 13:28:04 +0700

    refactor avatar

--- lib/src/models/contact.dart
+++ lib/src/models/contact.dart
@@ -2,6 +2,12 @@ class Contact {
   final String name;
   final String shortMessage;
   final String time;
+  final String image;
 
-  Contact({required this.name, required this.shortMessage, required this.time});
+  Contact({
+    required this.name,
+    required this.shortMessage,
+    required this.time,
+    required this.image,
+  });
 }
